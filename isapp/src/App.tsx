0bc9dd052c7e1d66a4c56315f3c04ac675cd80ee
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-09-08 20:36:27 +0900

    chore: PWD SETTING

--- isapp/src/App.tsx
+++ isapp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import AppList from './features/applist/AppList';
 import Header from './components/Header';
 import AppBrowser from './features/appbrowser/AppBrowser';
--- isapp/vite.config.ts
+++ isapp/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ISAapps',
+        short_name: 'Iapp',
+        start_url: '/iapp/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        description: 'A ISA apps.',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          { src: 'pwa-192x192.png', type: 'image/png', sizes: '192x192' },
+          { src: 'pwa-512x512.png', type: 'image/png', sizes: '512x512' },
+        ],
+      },
+    }),
+  ],
+});
