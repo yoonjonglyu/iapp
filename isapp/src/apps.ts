66cd475642ace0355b4e5814ff5afdedab691c4d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-09-28 03:42:45 +0900

    fix: IMAGE PATH

--- isapp/src/apps.ts
+++ isapp/src/apps.ts
@@ -3,7 +3,7 @@ export type AppsProps = { name: string; icon: string, uri: string; }[];
 
 const apps: AppsProps = [
   { name: 'Memo', icon: 'https://yoonjonglyu.github.io/memo/assets/apple-touch-icon-60x60.png', uri: 'https://yoonjonglyu.github.io/memo/' },
-  { name: 'Daoxin', icon: 'https://yoonjonglyu.github.io/daoxin/apple-touch-icon-60x60.png', uri: 'https://yoonjonglyu.github.io/daoxin/' },
+  { name: 'Daoxin', icon: 'https://yoonjonglyu.github.io/daoxin/pwa-64x64.png', uri: 'https://yoonjonglyu.github.io/daoxin/' },
 ];
 
 export default apps;
